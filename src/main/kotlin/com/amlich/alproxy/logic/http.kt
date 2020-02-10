4241761da66f78e2de7c0d806a03bce124f2b260
Van Nhu Nguyen <nguyen.van.nhu@vnmedia.se>
2020-02-10 21:40:25 +0100

    Using timeout

--- src/main/kotlin/com/amlich/alproxy/logic/http.kt
+++ src/main/kotlin/com/amlich/alproxy/logic/http.kt
@@ -3,10 +3,15 @@ package com.amlich.alproxy.logic
 import com.github.kittinunf.fuel.Fuel
 import com.github.kittinunf.fuel.core.Parameters
 import com.github.kittinunf.fuel.core.Request
+import kotlinx.coroutines.withTimeout
 
 const val AMLICH_BASE_URL = "https://am-lich.com"
+var AMLICH_HTTP_TIMEOUT_MILLISECS = 1500L
 
 suspend fun amlichGet(path: String, params: Parameters?): Request {
     val url: String = "${AMLICH_BASE_URL}${path}"
-    return Fuel.get(url, params)
+    return withTimeout(AMLICH_HTTP_TIMEOUT_MILLISECS) {
+        Fuel.get(url, params)
+    }
+
 }
