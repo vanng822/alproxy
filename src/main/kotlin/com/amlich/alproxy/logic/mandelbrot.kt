1e1cd3a7d7c100c2a515db2f4d5fa6fd77443ce9
Van Nhu Nguyen <nguyen.van.nhu@vnmedia.se>
2020-02-15 17:31:36 +0100

    Reduce coroutines and anomous func

--- src/main/kotlin/com/amlich/alproxy/logic/mandelbrot.kt
+++ src/main/kotlin/com/amlich/alproxy/logic/mandelbrot.kt
@@ -27,36 +27,34 @@ fun iternator(x: Double, y: Double): Int {
     }
 }
 
-data class InteratorResult(val i:Int, val newline: Boolean)
-
-fun createJob(x: Double, y: Double, newline: Boolean): Deferred<InteratorResult> {
-    return GlobalScope.async<InteratorResult> {
-        val i = iternator(x, y)
-        InteratorResult(i, newline)
-    }
-}
+data class InteratorResult(val results: MutableList<Int>)
 
 suspend fun generateMandelbrot(): String {
-    var jobs = mutableListOf<Deferred<InteratorResult>>()
+    val jobs = mutableListOf<Deferred<InteratorResult>>()
     for (y in -39 until 39) {
-        var newline = true
-        for (x in -39 until 39) {
-            val job = createJob(x.toDouble()/40.0, y.toDouble()/40.0, newline)
-            jobs.add(job)
-            newline = false
-        }
+        val job = fun(y:Double): Deferred<InteratorResult> {
+            return GlobalScope.async<InteratorResult> {
+                val results = mutableListOf<Int>()
+                for (x in -39 until 39) {
+                    results.add(iternator(x.toDouble()/40.0, y/40.0))
+                }
+                InteratorResult(results)
+            }
+        }(y.toDouble())
+
+        jobs.add(job)
     }
     val results = jobs.awaitAll()
 
     var output = "<pre>"
-    results.forEach{
-        if (it.newline) {
-            output += "\n"
-        }
-        if (it.i == 0) {
-            output += "*"
-        } else {
-            output += " "
+    results.forEach {
+        output += "\n"
+        it.results.forEach { i ->
+            if (i == 0) {
+                output += "*"
+            } else {
+                output += " "
+            }
         }
     }
     output += "</pre>"
